de87839fe49d45484e50f1fa219989bdc0c8a22d
jcgonzalezb <jcgonzalez@gmail.com>
2022-05-18 12:15:29 -0500

    for cycle implemented

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,19 @@
 This project module contains several fucntions
 to work with the personal data
 """
+import re
 
-def filter_datum():
-	
+def filter_datum(fields, redaction, message, separator):
+    #result = re.sub(fields, redaction, message)
+    for f in fields:
+        result = re.sub(f, redaction, message) 
+    """print(fields)
+    print(type(fields))
+    print(redaction)
+    print(type(redaction))
+    print(message)
+    print(type(message))
+    print(separator)
+    print(type(separator))
+    #return message"""
+    return result
