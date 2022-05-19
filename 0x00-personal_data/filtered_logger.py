a5af349426acff9a3ed990955ae627d31355fc98
jcgonzalezb <jcgonzalez@gmail.com>
2022-05-18 20:25:19 -0500

    filter updated

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,26 +1,10 @@
 #!/usr/bin/env python3
 """
-This project module contains several fucntions
-to work with the personal data
+This project module contains a logging module
 """
 import re
 
-def filter_datum(fields, redaction, message, separator):
-    #result = re.sub(fields, redaction, message)
-    print(message)
-    print(type(message))
-    for f in fields:
-        print(re.sub(f, redaction, message, flags=re.IGNORECASE))
-    #print(fields)
-    #print(type(fields))
-    #for f in fields:
-    #    print(f)
 
-    """print(redaction)
-    print(type(redaction))
-    print(message)
-    print(type(message))
-    print(separator)
-    print(type(separator))
-    #return message"""
-    #return result
+def filter_datum(fields, redaction, message, separator):
+    return (separator.join(x if x.split('=')[0] not in fields else re.sub(
+        r'=.*', '=' + redaction, x) for x in message.split(separator)))
