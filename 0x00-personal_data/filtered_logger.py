eb118628a6018663eee0c2a4812bec89ad00200f
jcgonzalezb <jcgonzalez@gmail.com>
2022-05-19 10:34:08 -0500

    format method updated

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,17 +8,23 @@ import logging
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
-        """
+    """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self):
+        """Initialize the class"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
 
     def format(self, record: logging.LogRecord) -> str:
-        NotImplementedError
+        """
+        Method to filter values in incoming log records using filter_datum.
+            Returns: A log.
+        """
+        msg = logging.Formatter(self.FORMAT).format(record)
+        return filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
 
 
 def filter_datum(fields, redaction, message, separator):
