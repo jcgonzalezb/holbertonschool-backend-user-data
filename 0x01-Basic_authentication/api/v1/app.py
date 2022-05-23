a46a3be9a1444a6f5e5fbe4313699360a4d612a7
jcgonzalezb <jcgonzalez@gmail.com>
2022-05-23 14:20:33 -0500

    forbidden implemented

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -27,6 +27,11 @@ def request_unauthorized(error) -> str:
     """
     return jsonify({"error": "Unauthorized"}), 401
 
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """ forbidden access handler
+    """
+    return jsonify({"error": "Forbidden"}), 403
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
