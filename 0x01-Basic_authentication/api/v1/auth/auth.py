9e1171ac703829e0b66cd04b660a5a9b6b543cd1
jcgonzalezb <jcgonzalez@gmail.com>
2022-05-23 17:44:43 -0500

    def require_auth updated

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,10 +11,25 @@ class Auth():
     """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Public method that requires authentication
-            Returns: False - path and excluded_paths
+        """Public method that define which routes don't need authentication.
+        This method must be slash tolerant.
+            Returns: 
+            True if the path is not in the list of strings excluded_paths.
+            True if path is None
+            True if excluded_paths is None or empty
+            False if path is in excluded_paths
+            excluded_paths contains string path always ending by a /
         """
-        return False
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+
+        if path[-1] != '/':
+            path = path + '/'
+
+        if path in excluded_paths:
+            return False
+        else:
+            return True
 
     def authorization_header(self, request=None) -> str:
         """Public method for authorization header
