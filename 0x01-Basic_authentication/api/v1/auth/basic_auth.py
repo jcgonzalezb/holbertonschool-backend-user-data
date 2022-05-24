9fc5e1a7600be9326009ea63ff3ae5bc12b525a7
jcgonzalezb <jcgonzalez@gmail.com>
2022-05-24 17:35:11 -0500

    testing

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,7 +3,8 @@
 Class BasicAuth
 """
 from api.v1.auth.auth import Auth
-
+import base64
+import codecs
 
 class BasicAuth(Auth):
     """flask authorization BasicAuth Class
@@ -16,8 +17,8 @@ class BasicAuth(Auth):
 
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
-        """ Method that extracts that takes the authorization header
-        and returns the Base64 part of the Authorization header
+        """ Method that takes the authorization header and
+        returns the Base64 part of the Authorization header
         for a Basic Authentication.
         """
 
@@ -28,3 +29,18 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ Method that takes the base64_authorization header and
+        returns the decoded value of a Base64 string
+        base64_authorization_header
+        """
+        if base64_authorization_header is None or not isinstance(
+        base64_authorization_header, str):
+            return None
+        
+        try:
+            return base64_authorization_header.decode('utf-8')
+        except UnicodeDecodeError:
+            return None
