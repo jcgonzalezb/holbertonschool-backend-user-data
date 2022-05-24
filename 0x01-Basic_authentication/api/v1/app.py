2389f12e7cc51a6605695d7c0c2233a5b557339c
jcgonzalezb <jcgonzalez@gmail.com>
2022-05-23 19:39:53 -0500

    before_request updated

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,11 +7,25 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request, make_response
 from flask_cors import (CORS, cross_origin)
 import os
+from api.v1.auth.auth import Auth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+auth_list = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+
+@app.before_request
+def before_request():
+    """Method that filters authorization to each request"""
+    if auth is not None:
+        check_auth = Auth.require_auth(request.path, auth_list)
+        if check_auth is True:
+            if auth.authorization_header(request) == None:
+                abort(401)
+            if auth.current_user(request) == None:
+                abort(403)
 
 
 @app.errorhandler(404)
