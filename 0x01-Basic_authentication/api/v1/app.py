b17c64f87776401045b3103ce1565512955ac4bf
jcgonzalezb <jcgonzalez@gmail.com>
2022-05-24 13:51:35 -0500

    basic_auth updated

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -15,9 +15,13 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 auth = os.getenv('AUTH_TYPE')
 
-if auth:
-    from api.v1.auth.auth import Auth
-    auth = Auth()
+if auth == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+else:
+    if auth:
+        from api.v1.auth.auth import Auth
+        auth = Auth()
 
 
 @app.errorhandler(404)
