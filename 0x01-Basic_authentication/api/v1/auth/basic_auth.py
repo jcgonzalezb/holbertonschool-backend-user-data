5a46e4094517a133b00ee6ca093e72befcbf436b
jcgonzalezb <jcgonzalez@gmail.com>
2022-05-24 13:43:41 -0500

    initial version

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Class BasicAuth
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """flask authorization BasicAuth Class
+    """
+
+    def __init__(self):
+        """ Constructor
+        """
+        pass
