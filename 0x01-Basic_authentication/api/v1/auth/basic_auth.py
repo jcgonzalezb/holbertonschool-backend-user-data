8da7908681063d883126150c4c99530a80d6d295
jcgonzalezb <jcgonzalez@gmail.com>
2022-05-24 15:02:12 -0500

    comment added

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -16,6 +16,11 @@ class BasicAuth(Auth):
 
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
+        """ Method that extracts that takes the authorization header
+        and returns the Base64 part of the Authorization header
+        for a Basic Authentication.
+        """
+
         if authorization_header is None or not isinstance(
                 authorization_header, str):
             return None
