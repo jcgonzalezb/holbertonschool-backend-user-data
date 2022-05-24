4b7450137e499711a94a8f1e4574bbd15dfdc0d4
jcgonzalezb <jcgonzalez@gmail.com>
2022-05-24 14:49:50 -0500

    base64_auth fixed

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 Class BasicAuth
 """
 from api.v1.auth.auth import Auth
+from flask import request
 
 
 class BasicAuth(Auth):
@@ -13,3 +14,16 @@ class BasicAuth(Auth):
         """ Constructor
         """
         pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        if authorization_header is None or not isinstance(
+                authorization_header, str):
+            return None
+
+        start_auth = authorization_header[:6]
+        base64_auth = authorization_header[6:]
+
+        if start_auth != "Basic ":
+            return None
+        return base64_auth
