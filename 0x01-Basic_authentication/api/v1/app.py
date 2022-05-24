2444b57a25367652237db239d05ef53f31b10a0f
jcgonzalezb <jcgonzalez@gmail.com>
2022-05-24 11:36:13 -0500

    before request fixed

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -45,13 +45,13 @@ def forbidden(error) -> str:
 def before_request():
     """Executed before each request that is handled by a function of the
     Blueprint."""
-    auth_list = [
+    excluded_paths = [
         '/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/'
     ]
     if auth is None:
         return
-    check_auth = auth.require_auth(request.path, auth_list)
-    if check_auth is False:
+    check_auth = auth.require_auth(request.path, excluded_paths)
+    if check_auth is True:
         return
     if auth.authorization_header(request) is None:
         abort(401)
