4dc7af96275140e557e57b2282511a890325bdc0
jcgonzalezb <jcgonzalez@gmail.com>
2022-05-26 18:02:39 -0500

    session_auth updated

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,19 +1,29 @@
 #!/usr/bin/env python3
 """ Module Flask view that handles all routes for the Session authentication.
 """
-from flask import jsonify, abort
-
+from flask import jsonify, request
+from api.v1.views import app_views
+from models.user import User
+from typing import TypeVar
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
-def login() -> str:
+def login():
     """ POST /auth_session/login
     Return:
-      - the status of the API
+      - Access to user
     """
-
-
-
-
-
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if email == '' or None:
+      return jsonify({"error": "email missing"}), 400
+    if password == '' or None:
+      return jsonify({"error": "password missing"}), 400
     
-    return jsonify({"status": "OK"})
+    user_credentials = {'email': email, }
+    user = User()
+    result = user.search(user_credentials)
+    if not result:
+        return jsonify({ "error": "no user found for this email" }), 404
+    user = result[0]
+    if not user.is_valid_password(password):
+        return jsonify({ "error": "wrong password" }), 401
