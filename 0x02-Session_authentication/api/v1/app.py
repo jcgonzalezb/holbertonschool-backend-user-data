2521de363f10c78b834539752e27d3044acb7132
jcgonzalezb <jcgonzalez@gmail.com>
2022-05-26 12:27:38 -0500

    app updated

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -62,11 +62,8 @@ def before_request():
     check_auth = auth.require_auth(request.path, excluded_paths)
     if check_auth is False:
         return
-    if auth.authorization_header(request) is None:
-        abort(401)
-    auth_header = auth.authorization_header(request)
-    auth_cookie = auth.session_cookie(request)
-    if auth_header is None and auth_cookie is None:
+    if auth.authorization_header(request) and \
+            auth.session_cookie(request) is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
