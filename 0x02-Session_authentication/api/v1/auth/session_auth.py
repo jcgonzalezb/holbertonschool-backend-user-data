c83b2756b0296ed7a4a7df8b70903a372f9227fd
jcgonzalezb <jcgonzalez@gmail.com>
2022-05-27 13:59:31 -0500

    destroy_session updated

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -51,3 +51,11 @@ class SessionAuth(Auth):
         """
         if request is None:
             return False
+        session_id = self.session_cookie(request)
+        if not session_id:
+            return False
+        user_id = self.user_id_for_session_id(session_id)
+        if not user_id:
+            return False
+        user = User.get(user_id)
+        user.remove(self)
