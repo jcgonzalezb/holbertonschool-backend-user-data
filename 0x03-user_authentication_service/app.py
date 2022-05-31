38e80f2f2ace4a28f05a5f4e5ff0f08d288abc6e
jcgonzalezb <jcgonzalez@gmail.com>
2022-05-31 15:44:33 -0500

    app file

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,8 +3,11 @@
 Route module for the API
 """
 from flask import Flask, jsonify
+from auth import Auth
+
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
@@ -12,6 +15,20 @@ def home() -> str:
     """ GET /
     Return:
       - Welcome message.
+    """
+    return jsonify({"message": "Bienvenue"})
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """ POST /users
+    JSON body:
+    - email
+    - password
+    - last_name (optional)
+    - first_name (optional)
+    Return:
+    - User object JSON represented
+    - 400 if can't create the new User
     """
     return jsonify({"message": "Bienvenue"})
 
