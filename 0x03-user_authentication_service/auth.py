8a250e95e910b95faf4b34621190814db6a7819f
jcgonzalezb <jcgonzalez@gmail.com>
2022-05-31 11:53:16 -0500

    initial version

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,6 +2,8 @@
 """DB module
 """
 import bcrypt
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -13,3 +15,30 @@ def _hash_password(password: str) -> bytes:
     bytePwd = password.encode('utf-8')
     mySalt = bcrypt.gensalt()
     return bcrypt.hashpw(bytePwd, mySalt)
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        """ Constructor
+        """
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Method should take mandatory email and password string arguments.
+            Returns: A User object.
+        """
+        if email and password:
+            user = DB.find_user_by(email)
+            if user is not None:
+                raise ValueError("User {} already exists".format(email))
+            print("user is None")
+            hashed_password = _hash_password(password)
+            hashed_password = str(hashed_password)
+            print(hashed_password)
+            print("hashed_password")
+            new_user = self._db.add_user(email, hashed_password)
+            return new_user
+        return None
