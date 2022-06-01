921451577a7e9567b14e6d9a214f974f8aaa9fb6
jcgonzalezb <jcgonzalez@gmail.com>
2022-06-01 11:49:05 -0500

    test

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,24 +1,21 @@
 #!/usr/bin/env python3
-"""
-Contains DB class to handle data.
+""" DB module to save and update databse
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+
 from user import Base, User
 
 
-class DB():
-    """
-    DB class to handle data
-    """
+class DB:
+    """ DB class """
 
     def __init__(self) -> None:
-        """Initialize a new DB instance
-        """
+        """ Initialize a new DB instance """
         self._engine = create_engine("sqlite:///a.db")
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
@@ -26,22 +23,14 @@ class DB():
 
     @property
     def _session(self) -> Session:
-        """Set and Return the user session.
-        """
+        """ Memoized session object """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """
-        Method that takes two arguments to save the user to the database.
-            Returns: A User object.
-        """
-        if email is None or not isinstance(email, str):
-            return None
-        if hashed_password is None or not isinstance(hashed_password, str):
-            return None
+        """ Adds user to db """
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
