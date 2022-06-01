a8e488f85de38a39025b14ab1f655a6c9cde3d20
jcgonzalezb <jcgonzalez@gmail.com>
2022-06-01 13:12:32 -0500

    testing create_session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -79,12 +79,10 @@ class Auth():
         Method takes an email string argument
             Returns: Session ID as a string.
         """
-        if email:
-            try:
-                user = self._db.find_user_by(email=email)
-                session_id = _generate_uuid()
-                self._db.update_user(user.id, session_id=session_id)
-                return session_id
-            except NoResultFound:
-                return None
-        return None
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
