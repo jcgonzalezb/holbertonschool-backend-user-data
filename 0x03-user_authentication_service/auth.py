ca0ba63ae0a9b46d58c5644ef0582a16d0ebd571
jcgonzalezb <jcgonzalez@gmail.com>
2022-06-01 14:19:42 -0500

    get_user_from_session_id fixed

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -87,3 +87,18 @@ class Auth():
                 return session_id
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """
+        Method takes a single session_id string argument.
+            Returns: The corresponding User or None.
+        """
+        if session_id is None:
+            try:
+                user = self._db.find_user_by(session_id=session_id)
+                if user is None:
+                    return None
+                return user
+            except NoResultFound:
+                return None
+        return None
