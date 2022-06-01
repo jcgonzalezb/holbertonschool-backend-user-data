76fa772c95a0f3a93392251da2397d9d4877b9be
jcgonzalezb <jcgonzalez@gmail.com>
2022-06-01 10:25:44 -0500

    add_user fixed

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -38,12 +38,11 @@ class DB:
         Method that takes two arguments to save the user to the database.
             Returns: A User object.
         """
-        if email and hashed_password:
-            user = User(email=email, hashed_password=hashed_password)
-            self._session.add(user)
-            self._session.commit()
-            return user
-        return None
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        return user
+
 
     def find_user_by(self, **kwargs) -> User:
         """
