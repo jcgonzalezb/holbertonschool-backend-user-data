a1273e677c9b7746e281974d46e4bb0877012992
jcgonzalezb <jcgonzalez@gmail.com>
2022-06-01 20:37:15 -0500

    return fixed

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -90,7 +90,7 @@ class Auth():
         except NoResultFound:
             return None
 
-    def get_user_from_session_id(self, session_id: str) -> str:
+    def get_user_from_session_id(self, session_id: str) -> Union[str, None]:
         """
         Method takes a single session_id string argument.
             Returns: The corresponding User or None.
