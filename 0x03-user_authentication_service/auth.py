23eba6367e287ebe0b89104122971ae10ce44b67
jcgonzalezb <jcgonzalez@gmail.com>
2022-06-02 10:28:31 -0500

    get_reset_password_token fixed

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -128,9 +128,9 @@ class Auth():
         """
         try:
             user = self._db.find_user_by(email=email)
-            if user is not None:
-                new_token = _generate_uuid()
-                self._db.update_user(user.id, reset_token=new_token)
-                return new_token
         except NoResultFound:
             raise ValueError
+
+        new_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=new_token)
+        return new_token
