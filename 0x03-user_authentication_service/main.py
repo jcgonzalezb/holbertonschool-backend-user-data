e04031333cffb528399dd8e07a7909dd3a2744f2
jcgonzalezb <jcgonzalez@gmail.com>
2022-06-06 16:54:54 -0500

    log_out fixed

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -24,6 +24,36 @@ def log_in_wrong_password(email: str, password: str) -> None:
     assert response.status_code == 401
 
 
+def log_in(email: str, password: str) -> str:
+    """ Asserts this function's corresponding API route. """
+    data = {'email': email, 'password': password}
+    response = requests.post(url + '/sessions', data=data)
+    assert response.status_code == 200
+    return response.cookies.get('session_id')
+
+
+def profile_unlogged() -> None:
+    """ Asserts this function's corresponding API route. """
+    response = requests.get(url + '/profile')
+    assert response.status_code == 403
+
+def profile_logged(session_id: str) -> None:
+    """ Asserts this function's corresponding API route. """
+    user_session = log_in(EMAIL, PASSWD)
+    cookies = {'session_id': user_session}
+    response = requests.get(url + '/profile', cookies=cookies)
+    assert response.status_code == 200
+
+def log_out(session_id: str) -> None:
+    """ Asserts this function's corresponding API route. """
+    user_session = log_in(EMAIL, PASSWD)
+    cookies = {'session_id': user_session}
+    response = requests.delete(url + '/sessions', cookies=cookies)
+    assert response.status_code == 200
+
+
+
+
 EMAIL = "guillaume@holberton.io"
 PASSWD = "b4l0u"
 NEW_PASSWD = "t4rt1fl3tt3"
@@ -33,3 +63,7 @@ if __name__ == "__main__":
 
     register_user(EMAIL, PASSWD)
     log_in_wrong_password(EMAIL, NEW_PASSWD)
+    profile_unlogged()
+    session_id = log_in(EMAIL, PASSWD)
+    profile_logged(session_id)
+    log_out(session_id)
